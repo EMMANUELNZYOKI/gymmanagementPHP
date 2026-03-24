7fcb0a3e0d7516a219c08a3fe1240fdeff073552
EMMANUELNZYOKI <emmanuelnzyoki777@gmail.com>
2026-03-24 11:55:28 +0300

    first commit

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,41 @@
+<nav class="ts-sidebar">
+    <ul class="ts-sidebar-menu">
+
+        <li class="ts-label">Main</li>
+        <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="#"><i class="fa fa-files-o"></i> Package Type</a>
+            <ul>
+                <li><a href="add-courses.php">Add Package Type</a></li>
+                <li><a href="manage-courses.php">Manage Package Type</a></li>
+            </ul>
+        </li>
+        <li><a href="#"><i class="fa fa-desktop"></i> Categories</a>
+            <ul>
+                <li><a href="create-room.php">Add a Category</a></li>
+                <li><a href="manage-rooms.php">Manage Categories</a></li>
+            </ul>
+        </li>
+
+        <li><a href="registration.php"><i class="fa fa-user"></i>Member Registration</a></li>
+        <li><a href="manage-students.php"><i class="fa fa-users"></i>Manage Member </a></li>
+        <li><a href="#"><i class="fa fa-files-o"></i> Complaints</a>
+            <ul>
+                <li><a href="new-complaints.php">New</a></li>
+                <li><a href="inprocess-complaints.php">In Process</a></li>
+                <li><a href="closed-complaints.php">Closed</a></li>
+                <li><a href="all-complaints.php">All</a></li>
+            </ul>
+        </li>
+
+        <li><a href="#"><i class="fa fa-desktop"></i> Feedback</a>
+            <ul>
+                <li><a href="feedbacks.php">All Feebacks</a></li>
+                <!-- <li><a href="feedback-report.php">Feedbacks Report</a></li> -->
+            </ul>
+        </li>
+
+
+        <li><a href="access-log.php"><i class="fa fa-file"></i>User Access logs</a></li>
+
+
+</nav>
